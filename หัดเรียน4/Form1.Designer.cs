48f5add13244a481598d3d753eef57d2ba1ebca3
panupongkongkate <61161511+panupongkongkate@users.noreply.github.com>
2020-04-19 12:41:58 +0700

    red color detection with emgu

--- หัดเรียน4/Form1.Designer.cs
+++ หัดเรียน4/Form1.Designer.cs
@@ -29,12 +29,113 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox1.Location = new System.Drawing.Point(255, 48);
+            this.imageBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(690, 614);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox2.Location = new System.Drawing.Point(976, 48);
+            this.imageBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(681, 614);
+            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
+            this.imageBox2.Click += new System.EventHandler(this.imageBox2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.button1.Location = new System.Drawing.Point(48, 32);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 123);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.button3.Location = new System.Drawing.Point(48, 193);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 105);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "process";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox1.Location = new System.Drawing.Point(41, 375);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 152);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(34, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 39);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "จำนวนที่นับได้";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1672, 722);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- หัดเรียน4/Form1.cs
+++ หัดเรียน4/Form1.cs
@@ -7,14 +7,140 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.Util;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.Runtime.InteropServices;
+using Emgu.CV.Features2D;
+
 
 namespace หัดเรียน4
 {
     public partial class Form1 : Form
     {
+        OpenFileDialog op = new OpenFileDialog();
+        Image<Bgr, byte> Image1 = null;
+        Image<Gray, byte> image2 = null;
+        int ku = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                //----รับภาพ--------------------------------------------------
+                Image1 = new Image<Bgr, byte>(op.FileName);
+                imageBox1.Image = Image1;
+
+            }
+        } //โหลด
+        private void imageBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Image1 != null)
+            {
+
+                //---แปลงเป็น hsv------------------------------------------------
+                Image<Hsv, byte> hsv = Image1.Convert<Hsv, byte>();
+
+
+                //แปลงเป็น ภาพเทา แบบ อาเรย์-Create result image----------------------------------------
+                Image<Gray, byte>[] channels = hsv.Split();
+
+                CvInvoke.InRange(channels[0], new ScalarArray(new MCvScalar(20)), new ScalarArray(new MCvScalar(160)), channels[0]);
+                //อันแรกคือ H อันสอง คือ S
+
+                channels[0]._Not();
+                channels[1]._ThresholdBinary(new Gray(100), new Gray(255));
+
+                //อันนี้ คือV new Gray(10)
+
+
+                IInputArray mask = null;
+                CvInvoke.BitwiseAnd(channels[0], channels[1], channels[0], mask);
+
+                image2 = channels[0].Canny(150,0);
+
+
+                //-------------------------วาดกรอบโดยการใช้contour---------------------------------
+                
+                Emgu.CV.Util.VectorOfVectorOfPoint contour = new Emgu.CV.Util.VectorOfVectorOfPoint();
+                // คือการประกาศตัวแปรcountor 
+
+                    Mat hier = new Mat();
+                CvInvoke.FindContours(image2, contour, hier, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+                //คือการค้นหา contours 
+
+
+         
+                //---------------------------------------------------------------------------
+                //ทำการวนลูป 
+                for(int i =0; i < contour.Size;i++ )
+                {
+                    double paramiter = CvInvoke.ArcLength(contour[i], true);
+                    Emgu.CV.Util.VectorOfPoint approx = new Emgu.CV.Util.VectorOfPoint();
+                    CvInvoke.ApproxPolyDP(contour[i],approx,0.04*paramiter,true);
+
+                    CvInvoke.DrawContours(Image1, contour, i, new MCvScalar(0, 0, 0), 6); // i บอกจำนวนที่วาด
+                    ku++;
+
+                    //-------------หาศูนย์กลาง--------------
+                    var moment = CvInvoke.Moments(contour[i]);
+                    int x = (int)(moment.M10 / moment.M00);
+                    int y = (int)(moment.M01/moment.M00);
+
+                    if(approx.Size == 3 )
+                    {
+                        CvInvoke.PutText(Image1, ". ", new Point(x,y),Emgu.CV.CvEnum.FontFace.HersheyScriptSimplex,1.5,new MCvScalar(0,0,0),10);
+                    }
+                    if (approx.Size == 5)
+                    {
+                        CvInvoke.PutText(Image1, ". ", new Point(x, y), Emgu.CV.CvEnum.FontFace.HersheyScriptSimplex, 1.5, new MCvScalar(0, 0, 0), 10);
+                    }
+                    if (approx.Size == 6)
+                    {
+                        CvInvoke.PutText(Image1, ". ", new Point(x, y), Emgu.CV.CvEnum.FontFace.HersheyScriptSimplex, 1.5, new MCvScalar(0, 0, 0), 10);
+                    }
+                    if (approx.Size > 6)
+                    {
+                        CvInvoke.PutText(Image1, ". ", new Point(x, y), Emgu.CV.CvEnum.FontFace.HersheyScriptSimplex, 1.5, new MCvScalar(0, 0, 0), 10);
+                    }
+                    imageBox2.Image = Image1;
+                }
+
+
+
+                textBox1.Text = ku.ToString();
+
+               ku = 0;
+            }
+        } //แดง
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
